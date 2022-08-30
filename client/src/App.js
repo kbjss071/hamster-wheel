84d7999bc36733d4fe234b0fad4e233cc7c92791
fairiberry <farrahsyed97@gmail.com>
2022-08-30 14:53:17 -0400

    login component
    worked on login component + started navigation bar.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import BMI from './components/ BMI'
+import BMI from './components/ BMI';
+import Login from './components/Login';
+import Navagation from './components/Navagation';
 
 function App() {
   return (
     <div style={{ backgroundColor: '#f4f1de', width: '100px', height: '100px'}} >
     <div className="App">
       <div>
-        <BMI />
+        <BMI /> <Navagation /> <Login />
       </div>
     </div>
     </div>
