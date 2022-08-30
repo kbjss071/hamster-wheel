235e80cd91c2c2911871807cd9a151881b72fb81
fairiberry <farrahsyed97@gmail.com>
2022-08-30 12:01:53 -0400

    liveshare 1
    collaborated on file structure (using live server)

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
