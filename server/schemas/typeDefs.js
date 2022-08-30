15f8dfb6dc500221ae77d2dd3510576728697f72
Jin Kang <kbjss071@gmail.com>
2022-08-29 23:25:43 -0400

    worked on schemas

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,44 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql `
+    type Exercise {
+        name: String!
+        bodyPart: String!
+        equipment: String!
+        target: String!
+    }
+
+    type User {
+        username: String!
+        email: String!
+        password: String!
+        fullname: String
+        savedExercise: [Exercise]
+    }
+
+    type Auth {
+        token: ID
+        user: User
+    }
+
+    type Query {
+        exercises(): [Exercise]
+        user: User
+    }
+
+    type Mutation {
+        addUser(
+            username: String!
+            email: String!
+            password: String!
+            fullname: String!
+        ): Auth
+        updateUser(
+            username: String
+            email: String
+            password: String
+            fullname: String
+        ): User
+        login(username: String!, password: String!): Auth
+    }
+`
