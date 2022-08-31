518d9db365abc70f8ad89b962d029dd0d723e05b
fairiberry <farrahsyed97@gmail.com>
2022-08-31 13:17:22 -0400

    fixed overlays
    NOW THEY'RE OKAY. FINALLY EVERYTHING DOES WHAT IT'S SUPPOSE TO T__T

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,25 +1,106 @@
-import React from "react";
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { Link } from 'react-router-dom';
+import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
-const Login = ({ loginPopup }) => {
-    return (
-        <div className="loginComponent">
+function Login({ loginPopup }) {
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN_USER);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const mutationResponse = await login({
+        variables: { email: formState.email, password: formState.password },
+      });
+      const token = mutationResponse.data.login.token;
+      Auth.login(token);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  return (
+    <div className="loginComponent">
         <div className={`${loginPopup ? "active" : ""} show`}>
             <div className="loginOverlay">
                 <div className="container">
-                    <form>
-                        <h1>Welcome Back!</h1>
-                        <p><input type="text" className="loginUser" placeholder="Username" />
-                        </p>
-                        <p><input type="text" className="loginUser" placeholder="Password" />
-                        </p>
-                        <button className="buttonMagic, linkbutton">Login</button>
-                    </form>
-                </div>
-            </div>
+      <Link to="/signup">← Go to Signup</Link>
+      <h2>Welcome Back!</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div className="flex-row space-between my-2">
+          <input
+          type="text"
+          className="loginUser"
+          placeholder="Username"
+          onChange={handleChange}
+          />
         </div>
+        <div className="flex-row space-between my-2">
+          <input
+          type="text"
+          className="loginUser"
+          placeholder="Password"
+          onChange={handleChange}
+          />
         </div>
-    )
-};
-
+        {error ? (
+          <div>
+            <p className="error-text">The provided credentials are incorrect</p>
+          </div>
+        ) : null}
+        <div className="flex-row flex-end">
+          <button
+          type="submit"
+          className="buttonMagic, linkbutton">Login</button>
+        </div>
+      </form>
+    </div>
+    </div>
+    </div>
+    </div>
+  );
+}
 
 export default Login;
+
+
+
+
+
+
+
+// import React from "react";
+
+// const Login = ({ loginPopup }) => {
+//     return (
+//         <div className="loginComponent">
+//         <div className={`${loginPopup ? "active" : ""} show`}>
+//             <div className="loginOverlay">
+//                 <div className="container">
+//                     <form>
+//                         <h1>Welcome Back!</h1>
+//                         <p><input type="text" className="loginUser" placeholder="Username" />
+//                         </p>
+//                         <p><input type="text" className="loginUser" placeholder="Password" />
+//                         </p>
+//                         <button className="buttonMagic, linkbutton">Login</button>
+//                     </form>
+//                 </div>
+//             </div>
+//         </div>
+//         </div>
+//     )
+// };
+
+
+// export default Login;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,27 @@
-import BMI from '../components/BMI';
+import React, { useState } from "react";
+import CheckBMI from '../components/BMI';
 import Exercises from '../components/Exercises';
 import Navigation from '../components/Navagation'
 import Login from '../components/Login'
 
 export default function Home () {
+
+    const [ loginPopup, setLoginPopup] = useState(true);
+    const [ bmiPopup, setBmiPopup] = useState(true);
+
+    const handleLoginClick = () => {
+        setLoginPopup((loginPopup) => !loginPopup);
+    };
+
+    const handleBMIClick = () => {
+        setBmiPopup((bmiPopup) => !bmiPopup);
+    }
     return (
         <div>
-            <BMI />
             <Exercises />
-            <Navigation />
-            <Login />
+            <Navigation handleLoginClick={handleLoginClick} />
+            <Login loginPopup={loginPopup} />
+            <CheckBMI handleBMIClick={handleBMIClick} bmiPopup={bmiPopup} />
         </div>
     )
 }
